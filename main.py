3e8ca2f8539484691e58a9df5515a99d2ab5c995
taogre <mr.tao.gre@gmail.com>
2017-03-07 16:33:56 +0100

    special needs kid (windows) compatible

--- main.py
+++ main.py
@@ -3,16 +3,29 @@
 import sys
 import string
 from collections import deque
+import os
 
-class colors:
-    HEADER = '\033[95m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    RED = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+if os.name == posix:
+    class colors:
+        HEADER = '\033[95m'
+        BLUE = '\033[94m'
+        GREEN = '\033[92m'
+        WARNING = '\033[93m'
+        RED = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+else:
+    class colors:
+        HEADER = ''
+        BLUE = ''
+        GREEN = ''
+        WARNING = ''
+        RED = ''
+        ENDC = ''
+        BOLD = ''
+        UNDERLINE = ''
+    
 
 ABC = list(string.ascii_uppercase)
 
